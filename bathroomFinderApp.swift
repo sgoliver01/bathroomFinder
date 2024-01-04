099e007a412fab5fbba0474212e1ce0fe733e9aa
sgoliver01 <109550283+sgoliver01@users.noreply.github.com>
2024-01-04 18:12:34 -0500

    Add files via upload
    beginning ot set up firecloud with google

--- bathroomFinderApp.swift
+++ bathroomFinderApp.swift
@@ -6,9 +6,20 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct bathroomFinderApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     @StateObject var locationManager = LocationManager()
     
     var body: some Scene {
