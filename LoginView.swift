0afaeb6f8e3fb4c5f3e873a5fabeebb40e522cb2
sgoliver01 <109550283+sgoliver01@users.noreply.github.com>
2024-01-09 12:01:30 -0500

    login checks email & password and disables buttons

--- LoginView.swift
+++ LoginView.swift
@@ -10,11 +10,16 @@ import SwiftUI
 import Firebase
 
 struct LoginView: View {
+    enum Field {
+        case email, password
+    }
+    
     @State private var email = ""
     @State private var password = ""
     @State private var showingAlert = false
     @State private var alertMessage = ""
-    
+    @State private var buttonsDisabled = true
+    @FocusState private var focusField: Field?
     
     var body: some View {
         NavigationStack {
@@ -29,9 +34,23 @@ struct LoginView: View {
                     .autocorrectionDisabled()
                     .textInputAutocapitalization(.never)
                     .submitLabel(.next)
+                    .focused($focusField, equals: .email) //bound to our email field
+                    .onSubmit {
+                        focusField = .password
+                    } //moves you to password
+                    .onChange(of: email, perform: { _ in
+                        enableButtons()
+                    })
                 SecureField("Password", text: $password)
                     .textInputAutocapitalization(.never)
                     .submitLabel(.done)
+                    .focused($focusField, equals: .password) //bound to our password field
+                    .onSubmit {
+                        focusField = nil //will dismiss the keyboard
+                    }
+                    .onChange(of: password, perform: { _ in
+                        enableButtons()
+                    })
             }
             .textFieldStyle(.roundedBorder)
             .overlay {
@@ -55,6 +74,7 @@ struct LoginView: View {
                 }
                 .padding(.leading)
             }
+            .disabled(buttonsDisabled)
             .buttonStyle(.borderedProminent)
             .font(.title2)
             .padding(.top)
@@ -66,6 +86,13 @@ struct LoginView: View {
         }
         
     }
+    
+    func enableButtons() {
+        let emailIsGood = email.count >= 6 && email.contains("@")
+        let passwordIsGood = password.count >= 6
+        buttonsDisabled = !(emailIsGood && passwordIsGood)
+    }
+    
     func register() {
         Auth.auth().createUser(withEmail: email, password: password) { result, error in
             if let error = error {  //means if error returns non nil when logging in
