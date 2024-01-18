37090c7d870b7a83d5851216dd53a19909374448
sgoliver01 <109550283+sgoliver01@users.noreply.github.com>
2024-01-17 22:31:07 -0500

    models with review up

--- Review.swift
+++ Review.swift
@@ -13,11 +13,16 @@ struct Review: Identifiable, Codable {
     @DocumentID var id: String?
     var title = ""
     var body = ""
+    var easeOfUse = ""
+    var easyToFind = ""
+    var line = ""
+    var toiletPaper = ""
+    var cleanliness = ""
     var rating = 0
     var reviewer = ""
     var postedOn = Date()
     
     var dictionary: [String: Any] {
-        return ["title": title, "body": body, "rating": rating, "reviewer": Auth.auth().currentUser?.email ?? "", "postedOn": Timestamp(date: Date())]
+        return ["title": title, "body": body, "easeOfUse": easeOfUse, "rating": rating, "easyToFind": easyToFind, "line": line, "toiletPaper": toiletPaper, "reviewer": Auth.auth().currentUser?.email ?? "", "postedOn": Timestamp(date: Date())]
     }
 }
